14ed283e3e8fe23e240622a9cc53943e94a58b45
Germany <49283890+GermanyVu@users.noreply.github.com>
2025-01-15 14:37:01 -0800

    Delete HW1 directory

--- HW1/simple_lin_reg.R
+++
@@ -1,8 +0,0 @@
-View(cars)
-plot(dist ~ speed, data = cars,
-     xlab = "Speed (in Miles Per Hour)",
-     ylab = "Stopping Distance (in Feet)",
-     main = "Stopping Distance vs Speed",
-     pch  = 20,
-     cex  = 2,
-     col  = "grey")
